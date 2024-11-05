6dab5658ea4984c56dd610ed2f1997d202e1bf92
Sungpyo <pwsusc10@naver.com>
2024-11-06 00:27:06 +0900

    feat: modify user type

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,14 +1,19 @@
 // user
 export type UserType = {
   id: number;
-  userName: string;
+  user_name: string;
   email: string;
   profile: string | null;
   chips: number;
-  handCounts: number;
+  gameLogs: HandIdsType;
   CREATED_AT: Date | null;
   UPDATED_AT: Date | null;
   DELETE_AT: Date | null;
+};
+
+export type HandIdsType = {
+  total: number;
+  logIds: number[];
 };
 
 // navBar
