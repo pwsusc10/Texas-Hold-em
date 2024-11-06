68473cf9c23bf64737c04d0f7e500d37b5f32441
Sungpyo <pwsusc10@naver.com>
2024-11-06 12:44:27 +0900

    feat: storybook 설정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,46 @@
+import path from 'path';
 import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-onboarding', '@storybook/addon-essentials', '@chromatic-com/storybook', '@storybook/addon-interactions'],
+  staticDirs: ['../public'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-onboarding', '@storybook/addon-interactions'],
   framework: {
     name: '@storybook/nextjs',
     options: {}
   },
-  staticDirs: ['../public']
+  docs: {
+    autodocs: 'tag'
+  },
+  webpackFinal: async config => {
+    if (!config.module || !config.module.rules) {
+      return config;
+    }
+
+    config.module.rules = [
+      ...config.module.rules.map(rule => {
+        if (!rule || rule === '...') {
+          return rule;
+        }
+
+        if (rule.test && /svg/.test(String(rule.test))) {
+          return { ...rule, exclude: /\.svg$/i };
+        }
+        return rule;
+      }),
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack']
+      }
+    ];
+
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@': path.resolve(__dirname, '../src')
+      };
+    }
+    return config;
+  }
 };
 export default config;
