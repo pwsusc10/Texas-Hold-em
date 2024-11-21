7a22d85d6704da53e36ac6b9df0c25a0ed32f5be
Sungpyo <pwsusc10@naver.com>
2024-11-21 17:15:08 +0900

    feat: modal component 추가

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,9 +1,20 @@
 import ModalPortal from './ModalPortal';
 
-export default function Modal({ children }: { children: React.ReactNode }) {
+type Props = {
+  children: React.ReactNode;
+  size: 'md' | 'lg';
+};
+
+export default function Modal({ children, size }: Props) {
   return (
     <ModalPortal>
-      <div className="fixed left-0 top-0 z-50 bg-modal w-full h-full">{children}</div>
+      <div className="fixed left-0 top-0 z-50 w-full h-full">
+        <div
+          className={`absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 ${size == 'lg' ? 'h-[70vh] w-[60vw]' : 'h-[60vh] w-[35vw]'} overflow-y-auto bg-secondary rounded-md border-2 border-deepdark divide-y divide-gray`}
+        >
+          {children}
+        </div>
+      </div>
     </ModalPortal>
   );
 }
