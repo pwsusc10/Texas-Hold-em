4be7acaf46cd263ff40ec10fc18c7c4e733a87f0
Sungpyo <pwsusc10@naver.com>
2024-12-02 03:59:33 +0900

    feat: game log mock ui추가

--- src/app/(dashboard)/my/log/page.tsx
+++ src/app/(dashboard)/my/log/page.tsx
@@ -1,5 +1,26 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 
 export default function page() {
-  return <div>page</div>;
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className="w-3/5 mx-auto">
+      <Tabs value={value} onChange={handleChange} centered textColor="inherit" variant="fullWidth">
+        <Tab label="pre-flop" />
+        <Tab label="flop" />
+        <Tab label="turn" />
+        <Tab label="river" />
+      </Tabs>
+      <p className="text-center font-semibold text-2xl pt-[4rem]">진행한 경기가 없습니다.</p>
+    </div>
+  );
 }
